a70665a0f6ffb018eec08248e6c56c9309f0ed5b
Devyn Johnson <dljohnson1313@gmail.com>
2025-02-19 21:19:35 -0600

    Create db folder. Create schema.sql for userData table. Create User model to send all required data to the userData table. Minor changes to player and team models to include data that will be displayed on front-end.

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,10 @@
-CREATE DATABASE friends_db;
+CREATE DATABASE statTrick_db;
+
+\c statTrick_db;
+
+CREATE TABLE userData (
+    username VARCHAR(255) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    favoriteTeam VARCHAR(255) NOT NULL
+);
