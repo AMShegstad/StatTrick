6f8c404fad62b9b80e9236a59ed0846f18dfbf97
Devyn Johnson <dljohnson1313@gmail.com>
2025-02-20 18:33:56 -0600

    Change branch to update main

--- src/components/SkaterComponent.tsx
+++ src/components/SkaterComponent.tsx
@@ -3,4 +3,11 @@ import { Team } from '../models/teamModel';
 import { teams } from '../classes/cities';
 
 const fetchSkaterData = async (skater: Skater, onDataFetched: (data: Skater) => void) => {
-    
+    try {
+        const response = await fetch('');
+        const data = await response.json();
+        const skaterDataFromApi = data.standings.find(
+            (record: { teamAbbrev: { default: string }; wins: number; losses: number; otLosses: number; points: number; gamesPlayed: number }) =>
+                record.teamAbbrev?.default === skater.triCode
+        );
+    }
